2abfc9cb8b24cb2accdf446a5ec7220047d1ce29
Renan <baratellarenan@gmail.com>
2022-07-08 14:01:45 -0300

    Implementção do get e post de temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://blogpessoalrenan.herokuapp.com/temas', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://blogpessoalrenan.herokuapp.com/temas', tema, this.token)
+  }
+
+}
